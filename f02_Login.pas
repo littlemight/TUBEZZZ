7aa9c60af9d707f32bfb3af1d58cdb78aa22a281
Muhammad Ayyub Abdurrahman <49425375+Ayyub29@users.noreply.github.com>
2019-04-16 17:32:32 +0700

    tes

--- f02_Login.pas
+++ f02_Login.pas
@@ -3,13 +3,35 @@ unit f02_login;
 interface
 uses
     csv_parser,
+    crt,
     user_handler;
 
 function login(var data_user : tabel_user) : user;
 function isLogin(var who_login : User) : Boolean;
+function tutupinInput : string;
 
 implementation
 
+function tutupinInput : string;
+    var
+    inp : Char;
+    temp : string;
+
+    begin
+    temp := '';
+    repeat
+      begin
+      inp := readkey;
+      if (inp <> #13) then
+        begin
+        temp := temp + inp;
+        write('*');
+        end;
+       end;
+    until (inp = #13);
+    writeln;
+    tutupinInput := temp;
+    end;
 
 function login(var data_user : tabel_user) : user;
     var
@@ -18,7 +40,7 @@ function login(var data_user : tabel_user) : user;
         i : integer;
     begin
         write('Masukkan username: '); readln(username);
-        write('Masukkan password: '); readln(password);
+        write('Masukkan password: '); password := tutupinInput();
         have_login := False;
         for i:= 1 to data_user.sz do
             begin
