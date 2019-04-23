d2b62c87ff80879fa63e6b63ee0542ee9dd001a3
littlemight <43075287+littlemight@users.noreply.github.com>
2019-04-23 21:00:22 +0700

    cleaned

--- main.pas
+++ main.pas
@@ -47,7 +47,7 @@ var
 
 { ALGORITMA }
 begin
-    // welcome();
+    welcome();
 	{ LOAD DATA KE ARRAY }
     writeln('Load file by writing "load"'); 
     write('$ '); 
@@ -60,7 +60,7 @@ begin
 	load(data_buku, data_user, data_peminjaman, data_pengembalian, data_kehilangan);
 	end_of_submenu(c);
 	clrscr();
-	// loading();
+	loading();
     end_of_submenu(c);
     
     { LOGIN PENGGUNA ATAU EXIT PROGRAM }
